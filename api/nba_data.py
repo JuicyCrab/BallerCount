b773c7ee7a0ebbd09f551c5b313ded5238738aef
JuicyCrab <EYASU.SMIEJA@GMAIL.COM>
2025-05-28 22:21:31 -0400

    nba_api stat retriever addition

--- api/nba_data.py
+++ api/nba_data.py
@@ -1,2 +1,22 @@
 #functions to pull player stats with the nba_api
-# use the st.cache.data in the streamlit for the get player stats 
+
+
+from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.static import players 
+
+
+def get_player_id(player_name):
+    find_player = players.find_players_by_full_name(player_name)
+    player_id = find_player[0]['id']
+    return player_id
+    
+def get_player_stats(player_name):
+    player_id = get_player_id(player_name)
+    if not player_id:
+        return None 
+    career = playercareerstats.PlayerCareerStats(player_id=player_id)
+    df = career.get_data_frames()[0]
+    if 'PLAYER_ID' in df.columns or 'TEAM_ID' in df.columns or 'LEAGUE_ID':
+        df = df.drop(columns=['PLAYER_ID', 'TEAM_ID', 'LEAGUE_ID'])
+    return df 
+   
